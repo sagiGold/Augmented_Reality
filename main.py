5c9091fcfcca7c21ce71422c396eb787efd18ef4
Idan Oren <idanor@mta.ac.il>
2021-12-30 02:39:01 +0200

    comments

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ def main():
     util.runVideo('IMG_8283.MOV','AR_Planar.avi',warping_perspective.processSingleFrame)
 
     # rgb,gray = util.import_frame('frame1.jpg')
-    # util.show_image(arp.processSingleFrame(rgb,gray),(10,10))
-    
+    # util.show_image(AR_planar.processSingleFrame(rgb,gray),(10,10))
+
+    # rgb,gray = util.import_frame('frame1.jpg')
+    # util.show_image(warping_perspective.processSingleFrame(rgb,gray),(10,10))
+
 if __name__ == "__main__":
     main()
     cv2.waitKey(0)
